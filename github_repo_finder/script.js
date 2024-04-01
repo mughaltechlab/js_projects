a2e296fc28e4ad837d1e85e437d1087530b6ad08
Saqib Ahmed <75667871+mughaltechlab@users.noreply.github.com>
2024-04-01 17:58:42 +0500

    github repo finder added

---
+++ github_repo_finder/script.js
@@ -0,0 +1,30 @@
+const card = document.querySelector('.card');
+const searchbtn = document.querySelector('.searchbtn');
+const searchInp = document.querySelector('.searchInp');
+
+searchbtn.addEventListener('click',()=>{
+    fetchGithubUser();
+});
+
+
+async function fetchGithubUser(){
+    const query = searchInp.value;
+    const url = `https://api.github.com/users/${query}`;
+    const resp = await fetch(url);
+    const user = await resp.json();
+
+    card.innerHTML = `<img src="${user.avatar_url}" alt="user's rofile image">
+    <div class="detail">
+        
+        <h1>${user.name}</h1>
+        
+        <p>Created at: ${user.created_at}</p>
+        <p>Followers: ${user.followers}</p>
+        <p>Following: ${user.following}</p>
+        <!-- repos -->
+        <p>Repo: ${user.public_repos}</p>
+        <!-- profile url -->
+        <p class="profBtn"><a href="${user.html_url}">Go to Profile</a></p>
+    </div>`;
+    console.log(user);
+}
